b8ecacfe7c2e07041e472a36e12303d5ea8dc213
KashimaKC <joshmaraun@gmail.com>
2022-04-16 20:39:03 -0700

    added landing page styling as well as a sample donation form on the "donate" page.

--- web-project/src/components/Home.js
+++ web-project/src/components/Home.js
@@ -1,19 +1,29 @@
 //NOTES:
 //  - definitely needs some more description in the home content.
 
+import Sponsors from "./Sponsors";
+
 
 const Home = () => {
+
+    const borderStyle = {
+        border: '3px solid #bbb',
+        borderRadius: '5px',
+        width: '95%'
+    };
 
 
     return (
         <div className="homepage-container">
             <div className="header-container">
-                <img src='https://static.wixstatic.com/media/c04eabff71f44db2a551eeeb9589a1f0.jpg/v1/fill/w_754,h_524,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c04eabff71f44db2a551eeeb9589a1f0.jpg'></img>
+                <img src='https://static.wixstatic.com/media/c04eabff71f44db2a551eeeb9589a1f0.jpg/v1/fill/w_754,h_524,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c04eabff71f44db2a551eeeb9589a1f0.jpg' alt=""></img>
                 <h2>
                     Starting Point is dedicated to supporting newly-arrived refugee children in the greater Sacramento area.
                     We strive to give them a warm welcome by providing basic necessities and programs as they begin their new lives in the U.S.
                 </h2>
             </div>
+            <hr style={borderStyle}/>
+            <Sponsors />
         </div>
     )
 }
---
+++ web-project/src/components/Sponsors.js
@@ -0,0 +1,43 @@
+
+const Sponsors = () => {
+
+    return (
+        <>
+            <h2 className="sponsor-header">
+                Thank you to the foundations, companies, and organizations that have supported our work in aiding refugee children from around the world!
+            </h2>
+
+            <div className="sponsors-flex">
+
+            <img src="https://static.wixstatic.com/media/6e56dc_68f719b0a39c4303bf4882a29c85a337~mv2.jpg/v1/fill/w_142,h_32,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/project%20linus_edited.jpg" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_68bc06ab2af64c9a8dbe386b9cc2eff8~mv2.jpg/v1/fill/w_195,h_38,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/sac-bee-logo.jpg" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_4b8030e6399243cbbc585a45620a0d2c~mv2.jpg/v1/fill/w_118,h_114,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/SRCF-Logo.jpg" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_9b706abbba3e488db207232d02ef72e3~mv2.jpg/v1/crop/x_152,y_140,w_656,h_478/fill/w_166,h_122,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/122057250_2637727756480666_8905557470952.jpg" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_140e1faaa0bd497fa56d8bfa6e471924~mv2.jpg/v1/fill/w_114,h_114,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/bombas.jpg" alt=""></img>
+
+            <img src="https://static.wixstatic.com/media/6e56dc_048e55e34ee04241987d03a75a293668~mv2.jpg/v1/fill/w_105,h_70,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/sacramento-republic-logo-soccer.jpg" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_c97ce0b5730d4c8a9b7b1a1b99626c86~mv2.jpg/v1/fill/w_84,h_78,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/thGYR7HQR8.jpg" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_bf82017b3f6d4ca3b8daa9ec6f863cf5~mv2_d_2300_1270_s_2.jpg/v1/fill/w_144,h_78,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/13087146_1051005571605208_29847575793532.jpg" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_a06613629c374b6490e9af0770efc512~mv2.png/v1/fill/w_135,h_46,al_c,usm_0.66_1.00_0.01,enc_auto/EIP%20-%20Sacramento%20Logo.png" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_584cdc257fb24560b06eb5d7ace706a7~mv2.jpg/v1/fill/w_119,h_84,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/KDP.jpg" alt=""></img>
+
+            <img src="https://static.wixstatic.com/media/6e56dc_19904351ff984ae187f191d0df2c667d~mv2.jpg/v1/fill/w_141,h_32,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/ins.jpg" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_5d9d115e641644ca9a47f0f2bdb5f116~mv2.png/v1/fill/w_143,h_46,al_c,usm_0.66_1.00_0.01,enc_auto/NxtGov_PNG.png" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_7ae6286429cb480a8acd07644bb1c235~mv2.jpg/v1/fill/w_134,h_94,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/15625a_529bd15ee3094616b839b08dcf00f3bb_.jpg" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_d4d3df89cd834b31b042d2c2273d4bc5~mv2.png/v1/fill/w_130,h_130,al_c,usm_0.66_1.00_0.01,enc_auto/ABAS.png" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_140297d33b024167b254c31f1c7be98d~mv2.jpg/v1/fill/w_158,h_78,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/OIP%20(1).jpg" alt=""></img>
+
+            <img src="https://static.wixstatic.com/media/6e56dc_819c8410d2e74eb6a9869ee2244f9c1e~mv2.jpg/v1/fill/w_113,h_130,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/OIP.jpg" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_afb1a1dffc8a43edaf517b4dc6d8d501~mv2.jpg/v1/crop/x_59,y_19,w_838,h_219/fill/w_184,h_46,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/95568030_2498371507082959_25624870791791.jpg" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_628a69cad3fe41b0a0f8a115fe597766~mv2.jpg/v1/fill/w_95,h_94,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/medium408.jpg" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_f573a044e31e43c6ad7bbf025ef24588~mv2.jpg/v1/fill/w_132,h_46,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/SQUATCHI-LOGO.jpg" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_b0121dbb4c9e405d9084c4af10b38e40~mv2.png/v1/fill/w_159,h_158,al_c,usm_0.66_1.00_0.01,enc_auto/spl_logo_2_jpg.png" alt=""></img>
+
+            <img src="https://static.wixstatic.com/media/6e56dc_40f1dd3cd9b94606b83fcb963d5d56e9~mv2.png/v1/crop/x_39,y_149,w_528,h_201/fill/w_231,h_88,al_c,usm_0.66_1.00_0.01,enc_auto/Screenshot%202021-04-09%20at%201_26_33%20PM.png" alt=""></img>
+            <img src="https://static.wixstatic.com/media/6e56dc_e2ac992702704c86a257cad705b33659~mv2.jpg/v1/fill/w_234,h_20,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/DB_LOGO-CMYK.jpg" alt=""></img>
+            </div>
+        </>
+    )
+}
+
+export default Sponsors
