95cb576a1f483499f96cc1c1be3c8fe477fc1381
Dominique Malgeri <dominique.malgeri400@myci.csuci.edu>
2022-05-07 10:25:14 -0700

    Children forms are now connected to the database

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql');
-//const { restart } = require('nodemon');
+const { restart } = require('nodemon');
 
 const app = express();
 
@@ -48,63 +48,47 @@ app.post('/', function (req, res) {
                             + "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         const sqlInsertChild = "INSERT INTO Children (child_id, parent_id, childFirstName, gender, age, shoe, childNotes) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        
-
-        var json = JSON.parse(children);
-
-        var obj1 = json[0];
-        var objVal1 = Object.values(obj1);
-        console.log("Child 1:", objVal1);
-
-        var obj2 = json[1];
-        var objVal2 = Object.values(obj2);
-        console.log("Child 2:", objVal2);
-
-        var obj3 = json[2];
-        var objVal3 = Object.values(obj3);
-        console.log("Child 3:", objVal3);
-
-        var obj4 = json[3];
-        var objVal4 = Object.values(obj4);
-        console.log("Child 4:", objVal4);
-
-        var obj5 = json[4];
-        var objVal5 = Object.values(obj5);
-        console.log("Child 1:", objVal5);
-
-        var obj6 = json[5];
-        var objVal6 = Object.values(obj6);
-        console.log("Child 1:", objVal6);
 
         //insert the parent into the database
-        // db.query(sqlInsertParent, ["", lastName, firstName, address, street, apartment, 
-        //                             city, zipcode, phone, dateOfArrival, dateReceived, 
-        //                             countryOfOrigin, immigrationStatus, speaksEnglish, notes], (err, result) => {
-        //                                 console.log(err);
-        // });
+        db.query(sqlInsertParent, ["", lastName, firstName, address, street, apartment, 
+                                    city, zipcode, phone, dateOfArrival, dateReceived, 
+                                    countryOfOrigin, immigrationStatus, speaksEnglish, notes], (err, result) => {
+                                        console.log(err);
+        });
         
         //Get the max parent_id from the Parent table (this will go into the Children table)
-        // db.query('SELECT max(parent_id) FROM Parent', function (error, results, fields) {
-        //     /*NOTE: The Parent table will never be null because even if it's empty at first, the insert
-        //     into the parent table comes before the insert into the child table.*/
-        //     if (error) {
-        //         throw error;
-        //     }
-        //     //Convert the result into a number: 
-        //     var s = JSON.stringify(results);
-        //     var json = JSON.parse(s);
-        //     var obj = json[0];
-        //     objVal = Object.values(obj);
-        //     var maxParentID = Number(objVal);
+        db.query('SELECT max(parent_id) FROM Parent', function (error, results, fields) {
+            /*NOTE: The Parent table will never be null at this query because even if it's empty at first, the insert
+            into the parent table comes before the insert into the child table.*/
+            if (error) {
+                throw error;
+            }
+            //Convert the result into a number: 
+            var s = JSON.stringify(results);
+            var json = JSON.parse(s);
+            var obj = json[0];
+            var objVal = Object.values(obj);
+            var maxParentID = Number(objVal);
 
-        //     //Insert the children into the database
-        //     //https://stackoverflow.com/questions/51193704/react-input-setstate-for-array-of-objects
-        //     // for(let i = 0; i < childField.length; i++) {
-        //     //     db.query(sqlInsertChild, ["", maxParentID,childField[i].childFirstName, childField[i].gender, childField[i].age, 
-        //     //                                 childField[i].shoe, childField[i].childNotes], (err, result) => {
-        //     //                                 console.log(err);
-        //     //     });
-        //     // }
-        // });
+            //Insert the children into the database
+            var jsonChildren = JSON.parse(children);
+            for(let i = 0; i < 6; i++) {
+                let objChildren = jsonChildren[i];
+                let objValChildren = Object.values(objChildren);
+                
+                //check if the child is null- if so, skip inserting the child into the databse
+                let isNull = false;
+                if(objValChildren[0] == null && objValChildren[1] == null && objValChildren[2] == null && objValChildren[3] == null && objValChildren[4] ==null)
+                    isNull = true;
+
+                if(isNull == false) {
+                    db.query(sqlInsertChild, ["", maxParentID, objValChildren[0], objValChildren[1], objValChildren[2], 
+                                                    objValChildren[3], objValChildren[4]], (err, result) => {
+                                                    console.log(err);
+                    });
+                }
+
+            }
+        });
     });
 });
